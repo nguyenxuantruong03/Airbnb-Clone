0f418e78c482462e07381c8682893ff356cc366c
asdiop159752 <nxt159753@gmail.com>
2023-06-21 08:07:20 +0700

    Fetching listings with server components(Listing card component, direct server action)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Truong</div>;
+import Container from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listing/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings =  await getListings();
+  const currentUser= await getCurrentUser();
+
+  if(listings.length === 0) {
+    return(
+    <ClientOnly>
+      <EmptyState showReset/>
+    </ClientOnly>
+    )
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="
+        pt-28 
+        grid 
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+        ">
+          {listings.map((listing:any)=>{
+            return (
+              <ListingCard 
+              currentUser={currentUser}
+              key={listing.key}
+              data={listing}
+              />
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  )
 }
